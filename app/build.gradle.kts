274f5d20e958db1093e7a8c036dfc708144e3f5e
jeoungda12 <37054366+jeoungda12@users.noreply.github.com>
2023-10-24 17:56:30 +0900

    231024

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.a2023_android_project"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.a2023_android_project"
