05b36b00df8c385cdf887b1f35b7d3e0d127d086
jeoungda12 <37054366+jeoungda12@users.noreply.github.com>
2023-10-27 17:59:55 +0900

    231027
    - 이미지 자료 추가
    - 네이버 지도 API 연동
    - 위치 permission 추가
    - Main_activity 작성

--- app/src/main/java/com/example/a2023_android_project/MainActivity.kt
+++ app/src/main/java/com/example/a2023_android_project/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.a2023_android_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.NonNull
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mapView: MapView
+    private val LOCATION_PERMISSTION_REQUEST_CODE: Int = 1000
+    private lateinit var locationSource: FusedLocationSource // 위치를 반환하는 구현체
+    private lateinit var naverMap: NaverMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    override fun onMapReady(p0: NaverMap) {
+        TODO("Not yet implemented")
+    }
+
 }
