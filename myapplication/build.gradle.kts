2c08731cca13687c13148114ad6a758136883a52
jeoungda12 <37054366+jeoungda12@users.noreply.github.com>
2023-10-31 18:01:43 +0900

    231031
    firebase 적용중..

--- myapplication/build.gradle.kts
+++ myapplication/build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jetbrains.kotlin.storage.CacheResetOnProcessCanceled.enabled
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
 }
 
 android {
@@ -33,6 +36,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    viewBinding {
+        enable = true
+    }
 }
 
 dependencies {
@@ -44,4 +50,18 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("com.google.firebase:firebase-bom:30.4.1")
+    implementation ("com.google.android.gms:play-services-auth:20.3.0")
+    implementation ("androidx.multidex:multidex:2.0.1")
+    implementation ("com.google.firebase:firebase-auth-ktx:21.0.8")
+
+    implementation ("com.google.firebase:firebase-firestore-ktx:24.3.0")
+    implementation ("com.google.firebase:firebase-storage-ktx:20.0.2")
+    implementation ("com.github.bumptech.glide:glide:4.13.2")
+    implementation ("com.firebaseui:firebase-ui-storage:8.0.1")
+    implementation ("com.github.bumptech.glide:compiler:4.13.2")
+
+    implementation ("com.google.firebase:firebase-messaging-ktx:23.0.8")
+    implementation ("com.google.firebase:firebase-analytics-ktx:21.1.1")
 }
