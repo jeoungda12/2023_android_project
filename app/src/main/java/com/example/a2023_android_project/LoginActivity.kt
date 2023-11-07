e557bddd7b8069bb2b42b248970b78981329db79
jeoungda12 <37054366+jeoungda12@users.noreply.github.com>
2023-11-07 22:26:02 +0900

    231107
    231107
    1. 이미지 추가
    drawable 머디 이미지 추가
    
    2. font 추가
    
    3. login.xml
    로그인 UI/UX 변경 및 추가

--- app/src/main/java/com/example/a2023_android_project/LoginActivity.kt
+++ app/src/main/java/com/example/a2023_android_project/LoginActivity.kt
@@ -17,21 +17,21 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding_login.root)
 
         //아이디 및 비밀번호 값 잃어오기
-        binding_login.loginButton.setOnClickListener{
-            Log.d("login","login Button Event !")
-            var login_id : String = binding_login.loginId.text.toString()
-            var login_password : String = binding_login.loginPassword.text.toString()
-            Log.d("login","login id : ${login_id} login password : ${login_password}")
-
-            if(login_id == "" || login_password == ""){
-                Log.d("login", "아이디, 비밀번호 입력안됨")
-                Toast.makeText(this, "아이디, 비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
-            }
-            else{
-                Log.d("login", "MainActivity로 이동합니다.")
-                startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-            }
-        }
+//        binding_login.loginButton.setOnClickListener{
+//            Log.d("login","login Button Event !")
+//            var login_id : String = binding_login.loginId.text.toString()
+//            var login_password : String = binding_login.loginPassword.text.toString()
+//            Log.d("login","login id : ${login_id} login password : ${login_password}")
+//
+//            if(login_id == "" || login_password == ""){
+//                Log.d("login", "아이디, 비밀번호 입력안됨")
+//                Toast.makeText(this, "아이디, 비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
+//            }
+//            else{
+//                Log.d("login", "MainActivity로 이동합니다.")
+//                startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+//            }
+//        }
 
         binding_login.loginFindPassword.setOnClickListener{
             Log.d("login", "비밀번호 찾기 화면으로 이동합니다")
